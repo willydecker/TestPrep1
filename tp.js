318a7bb9a52327a207ae1c01b007b81fc7b10c82
willydecker <wdw1062@saintjoe.edu>
2015-02-13 12:12:39 -0500

    works

---
+++ tp.js
@@ -0,0 +1,16 @@
+function range(start, end, step) {
+		if (step == null) step = 1;
+			  	var array = [];
+
+					if (step > 0) {
+								for (var i = start; i <= end; i += step)
+													array.push(i);
+									} 
+						else {
+									for (var i = start; i >= end; i += step)
+													array.push(i);
+										}
+							return array;
+}
+console.log(range(1, 10, 1));
+console.log(range(1, 10, 2));
